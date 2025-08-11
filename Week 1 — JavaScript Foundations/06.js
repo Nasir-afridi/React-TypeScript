a613d68a2e9da8901ce8953f051900625acc5a85
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 16:52:14 +0500

    find the cube using the arrow function

--- Week 1 — JavaScript Foundations/06.js
+++ Week 1 — JavaScript Foundations/06.js
@@ -6,8 +6,16 @@ hello();
 
 
 // 2.
-const sum = (a, b) => {
+const sum = function (a, b) {
     return a + b
 }
 let result = sum(4, 5)
 console.log(result)
+
+
+// 3.
+const cube = (n) => {
+    return n * n * n
+}
+let res = cube(2)
+console.log(res)
