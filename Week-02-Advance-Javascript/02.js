55f2850782836b29e57c72ff66555c7ad7e96235
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 23:10:57 +0500

    added the day-02 hoisting tasks

---
+++ Week-02-Advance-Javascript/02.js
@@ -0,0 +1,22 @@
+// 1.
+console.log(ame)
+var ame = "Nasir"
+
+
+// 2.
+console.log(age)
+let age = 20;
+
+
+// 3.
+nme()
+function nme() {
+    console.log("Nasir")
+}
+
+
+// 4.
+hi()
+const hi = () => {
+    console.log("ki")
+}
