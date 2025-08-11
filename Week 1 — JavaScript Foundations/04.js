f62a5ec085619491b690115e2762cad53910762b
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 14:40:16 +0500

    Dynamic Property Access

--- Week 1 — JavaScript Foundations/04.js
+++ Week 1 — JavaScript Foundations/04.js
@@ -4,6 +4,16 @@ let object = {
     age : 20, 
     grade : "A+"
 }
-
 console.log(object.name)
 console.log(object["grade"])
+
+
+// 2.
+let car = {
+    brand : "Toyota", 
+    model : 21,
+    year : 21
+}
+let prop = "model"
+
+console.log(car[prop])
