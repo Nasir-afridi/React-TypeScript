e9b614e86784e27c4aa89fe89146f35787d3433f
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 16:25:49 +0500

    task-02 array destructuring added

--- Week 1 — JavaScript Foundations/05.js
+++ Week 1 — JavaScript Foundations/05.js
@@ -6,3 +6,9 @@ let person = {
 }
 const {Name, year, country} = person
 console.log(Name, year, country)
+
+
+// 2.
+let color = ["red", "pink", "Yellow"]
+const [a, b, c] = color
+console.log(a, b, c)
