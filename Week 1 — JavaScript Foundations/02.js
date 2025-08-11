e1812b02207f560b43fe343f3974a30471b23af7
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 12:53:37 +0500

    task-02 palindrome check add

--- Week 1 — JavaScript Foundations/02.js
+++ Week 1 — JavaScript Foundations/02.js
@@ -4,3 +4,11 @@ function factorail(n) {
     return n
 }
 console.log(factorail(2))
+
+
+//2.
+function palindrome(name) {
+    let str = name.split('').reverse('').join('')
+    return name === str
+}
+console.log(palindrome(""))
