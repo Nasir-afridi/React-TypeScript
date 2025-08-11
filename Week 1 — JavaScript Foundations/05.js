fe7c65d275779336b0128ef159944e8dd90d7fc6
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 16:28:22 +0500

    added the task 3 spread opeartor

--- Week 1 — JavaScript Foundations/05.js
+++ Week 1 — JavaScript Foundations/05.js
@@ -12,3 +12,10 @@ console.log(Name, year, country)
 let color = ["red", "pink", "Yellow"]
 const [a, b, c] = color
 console.log(a, b, c)
+
+
+// 3.
+let arr1 = [1, 2, 3, 4]
+let arr2 = [5, 6, 7, 8]
+let final = [...arr1 , ...arr2]
+console.log(final)
