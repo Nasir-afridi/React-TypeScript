be6c8b885abb0d419bc34b4578f8b49d5bcfc73a
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 14:15:24 +0500

    task-03 square of the numbers added using map

--- Week 1 — JavaScript Foundations/03.js
+++ Week 1 — JavaScript Foundations/03.js
@@ -9,3 +9,12 @@ let arr = [1, 2, 3, 4, 5, 6]
 let result = arr.filter((even) => even % 2 === 0)
 console.log(result)
 
+//3.
+let square = [2, 4, 6]
+let sqr = square.map((sqr) => sqr * sqr)
+console.log(sqr)
+
+//4.
+let red = [10, 20, 30, 40]
+let res = red.reduce((acc, curr) => acc + curr)
+console.log
