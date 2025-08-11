eb93e52a219339f2d4cf7618a402734982aacf69
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 14:14:13 +0500

    added the task-02 even number using filter

--- Week 1 — JavaScript Foundations/03.js
+++ Week 1 — JavaScript Foundations/03.js
@@ -3,3 +3,9 @@ let fruits = ["Apple", "banana", "Mango"]
 fruits.push("watermelon")
 fruits.pop("watermelon")
 console.log(fruits)
+
+//2.
+let arr = [1, 2, 3, 4, 5, 6]
+let result = arr.filter((even) => even % 2 === 0)
+console.log(result)
+
