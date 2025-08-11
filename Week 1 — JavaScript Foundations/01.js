ced9cae0a413eb7da41fa99e2adbdb38caa1c688
Nasir-afridi <nasirwc05@gmail.com>
2025-08-11 12:37:32 +0500

    added day-1 variables, datatypes, operator tasks added

---
+++ Week 1 — JavaScript Foundations/01.js
@@ -0,0 +1,22 @@
+//1.
+const firstName = "Muhammad Nasir"
+const age = 20
+console.log(`My name is ${firstName} and My age is ${age}`)
+
+//2.
+let a = 5;
+let b = 10;
+a = a + b
+b = a - b
+a = a - b
+console.log(a, b)
+
+//3.
+let radius = 5;
+console.log("Area is ", 3.14 * 5 * 5)
+
+//4.
+const arr = ["red", true, 23]
+console.log(typeof(arr[0]))
+console.log(typeof(arr[1]))
+console.log(typeof(arr[2]))
